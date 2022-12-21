4c59cd2c26f5d60edf7d7715b1736c2df1bbf48d
Luis <luiscarlossfaria@gmail.com>
2022-12-20 23:28:24 -0300

    Updated deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const ERC721 = await hre.ethers.getContractFactory("FariaToken");
+  ERC721 = await ERC721.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await ERC721.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Contract deployed to ${ERC721.address}`
   );
 }
 
